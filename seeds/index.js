aa066d9c2fca627b4d2c82aa3072c2bfca81e484
Shelby <chaixiangzhi8262@gmail.com>
2023-08-15 11:19:08 +0100

    Add styles and layouts bootstrap

--- seeds/index.js
+++ seeds/index.js
@@ -17,9 +17,14 @@ const seedDB = async () => {
     await Campground.deleteMany({});
     for (let i = 0; i < 50; i++) {
         const random1000 = Math.floor(Math.random() * 1000);
+        const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
-            location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
+            location: `${cities[random1000].city}, ${cities[random1000].state}`,
+            image: "https://source.unsplash.com/collection/483251",
+            description:
+                "Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo, deleniti, necessitatibus quod incidunt accusamus ea reiciendis blanditiis fugit fugiat corrupti delectus illo obcaecati id mollitia unde ad maiores minus hic.",
+            price,
         });
         await camp.save();
     }
